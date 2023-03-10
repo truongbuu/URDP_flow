efd0a9ca13646b6f83236dbeebefe32a769212ea
truongbuu <truongbuu94@gmail.com>
2023-03-10 03:29:42 -0500

    change to Adam

--- train_universal.py
+++ train_universal.py
@@ -183,9 +183,9 @@ def main():
     mse = torch.nn.MSELoss()
 
     #discriminator.train()
-    opt_ssf= torch.optim.RMSprop(ssf.parameters(), lr=5e-4)
-    opt_d = torch.optim.RMSprop(discriminator.parameters(), lr=2e-4)
-    opt_dm = torch.optim.RMSprop(discriminator_M.parameters(), lr=2e-4)
+    opt_ssf= torch.optim.Adam(ssf.parameters(), lr=5e-4)
+    opt_d = torch.optim.Adam(discriminator.parameters(), lr=2e-4)
+    opt_dm = torch.optim.Adam(discriminator_M.parameters(), lr=2e-4)
     
     list_opt = [opt_ssf, opt_d, opt_dm]
     
